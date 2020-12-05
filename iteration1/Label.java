60295da59029f7ccc3ea0ae0cb155f1d3f3aa89f
hafsaaulusall <54844226+hafsaaulusall@users.noreply.github.com>
2020-12-05 18:03:25 +0300

    comments have been done

--- iteration1/Label.java
+++ iteration1/Label.java
@@ -1,20 +1,25 @@
-class Label{
-    private long LabelID;
+public class Label {
+    //Label class is created
+    private int LabelID;
     private String LabelText;
+    //attributes are defined
 
-    public void setLabelID(long id){
+    public void setLabelID(int id){
         this.LabelID=id;
+        //a set method is created to set the value of a specific variable LabelID
     }
-    public long getLabelID(){
+    public int getLabelID(){
         return LabelID;
+        //a get method is created to retrieve the value of a specific variable LabelID within a class
     }
 
     public void setLabelText(String text){
         this.LabelText=text;
+        //a set method is created to set the value of a specific variable LabelText
+
     }
     public String getLabelText(){
         return LabelText;
+        //a get method is created to retrieve the value of a specific variable LabelText within a class
     }
-
-
 }
