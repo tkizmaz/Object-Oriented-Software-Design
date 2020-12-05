98d32c1f90a919834638e0b24ca94164f5306652
tahakizmaz <tahakizmaz@hotmail.com>
2020-12-05 18:48:52 +0300

    Changes

--- iteration1/Main.java
+++ iteration1/Main.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
+        List<List<AssignedLabel>> assignedLabel=new ArrayList<>();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter filename");
+        String fileName = myObj.nextLine();
+        JSONHandler readJS = new JSONHandler();
+        readJS.readDataset(fileName);
+        readJS.readUsers("users.json");
+        readJS.getDataset().getUsers().forEach(user->{
 
-        System.out.println("Hello world.");
-       
+            user.makeAssignment(readJS.getDataset().getLabels(), readJS.getDataset().getInstances(),readJS.getDataset().getMaximumLabels());
+            assignedLabel.add(user.getAssignments());
+        });
+        
+        readJS.writeJSON("sampleOutput2.json", assignedLabel);
+        
     }
+
 }
