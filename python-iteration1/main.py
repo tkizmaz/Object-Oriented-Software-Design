9f80c92abd93b81b90f2b8bb8e629e6167bda83f
ZeynepAkyokus <56530792+ZeynepAkyokus@users.noreply.github.com>
2021-01-15 17:39:44 +0300

    FileHandler-Main

--- python-iteration1/main.py
+++ python-iteration1/main.py
@@ -1,5 +1,9 @@
 from Student import *
+from FileHandler import *
 
-emre=Student()
-emre.setStudentEmail("rock_emrbrky")
-print(emre.getStudentEmail())
+# emre=Student()
+# emre.setStudentEmail("rock_emrbrky")
+# print(emre.getStudentEmail())
+
+fileHandler=FileHandler()
+fileHandler.readStudentFile("CES3063_Fall2020_rptSinifListesi.XLS")
